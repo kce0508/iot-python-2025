a61292a05c0edc330f143d5001395216a2e66955
kce0508 <kce0508@gmail.com>
2025-03-16 15:57:06 +0900

    Update py04_stringformat.py

--- day02/py04_stringformat.py
+++ day02/py04_stringformat.py
@@ -4,7 +4,7 @@
 loginTemp = '안녕하세요, %s님!'
 # name = '유고'
 
-# #print(loginTemp % (name))
+# print(loginTemp % (name))
 # name = input('로그인할 이름 입력 > ')
 # print(loginTemp % (name))
 
@@ -16,7 +16,7 @@ intro = '나는 %10s(이)고, %03d살입니다. 몸무게 %3.1fkg입니다.'
 print(intro %('유고', 47, 76.4))
 
 ## 중간세대 문자열 포맷팅
-# {0:>10s}로 %10s와 동일하게 적용
+## {0:>10s}로 %10s와 동일하게 적용
 intro = '나는 {0}이고, {1}살입니다. 몸무게는 {2}kg입니다.'
 print(intro.format('육오', 48, 65.5))
 
